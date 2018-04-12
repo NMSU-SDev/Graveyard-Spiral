9f10dac8a27e6a606491d769274976f2cfff8499
shanegrayson <shaneg@nmsu.edu>
2018-04-12 15:03:27 -0600

    James' Checkuser Function

---
+++ checkuser/app/src/main/java/com/example/james/checkuser/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.james.checkuser;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Welcome to PTBuddy");
+                builder.setMessage("Are you already a member?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "this needs to go to login", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "This needs to go to user input", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.create().show();
+
+            }
+        });
+
+
+
+    }
+
+
+
